5d9f2dd07818667b323790b0574f367df5c749e5
Brandon <name.brandon.aguero@gmail.com>
2023-08-26 19:52:03 -0500

    Se agregaron los estilos correspondientes. Ahora es responsive para dispositovos pequeños, medianos y grandes

--- src/App.tsx
+++ src/App.tsx
@@ -30,13 +30,34 @@ function App(): JSX.Element {
 
   return (
     <>
-      <div
-        className="h-screen w-full bg-cover bg-no-repeat"
+      <main
+        className="flex h-screen max-h-[62.5rem] min-h-[30rem] w-full min-w-[20rem] max-w-[125rem] flex-col items-center justify-center gap-12 bg-cover bg-no-repeat font-roboto"
         style={{ backgroundImage: `url(${currentImage})` }}
       >
-        <h1 className="text-2xl">{currentPhrase}</h1>
-        <button onClick={changePhraseAndBackground}>Cambiar</button>
-      </div>
+        <h1
+          className="text-center text-3xl uppercase tracking-wider text-gray-50"
+          style={{ textShadow: "0.3rem 0.4rem 0.2rem rgb(0, 0, 0, 1)" }}
+        >
+          Galletas de la Fortuna
+        </h1>
+        <div
+          className="flex h-1/5 min-h-[10.75rem] w-5/6 flex-col items-center justify-center bg-contain bg-center bg-no-repeat"
+          style={{
+            backgroundImage:
+              "url('https://res.cloudinary.com/dbbixakcl/image/upload/f_auto,q_auto/v1/React/GalletaFortuna/cmzhkwtza83wqfcmconc')",
+          }}
+        >
+          <p className="w-[90%] max-w-[24rem] px-2 text-center text-xs sm:text-lg">
+            {currentPhrase}
+          </p>
+        </div>
+        <button
+          className="rounded-lg border-2 border-gray-50 bg-transparent px-8 py-2 text-gray-50"
+          onClick={changePhraseAndBackground}
+        >
+          Cambiar
+        </button>
+      </main>
     </>
   );
 }
